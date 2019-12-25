b6e8eb5d543b9a14a2a8f6b63688d3f286c3f6b3
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2019-12-25 19:08:20 +0530

    Extracting saved comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import praw
+import pandas as pd
 
 url = 'https://www.reddit.com/'
 with open('credentials.json') as f:
@@ -11,7 +12,20 @@ reddit = praw.Reddit(client_id=params['client_id'],
                      user_agent='Test App for pulling saved comments accessAPI:v0.0.1 (by /u/priya90r)',
                      username=params['username'])
 
-subreddit = reddit.subreddit('machinelearning')
-print(subreddit.display_name)
-print(subreddit.title)
-print(subreddit.description)
+s = reddit.redditor(params['username']).saved(limit=None)
+
+posts = []
+i = 0
+
+for post in s:
+    try:
+        i += 1
+        posts.append(
+            [post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+    except AttributeError as err:
+        print(err)
+    if i % 5 == 0:
+        print(f'{i} submissions completed')
+saved_df = pd.DataFrame(posts, columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+
+saved_df.to_csv("20191225_saved_comments_{}.csv".format(params['username']))
